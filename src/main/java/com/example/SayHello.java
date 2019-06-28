42fb5d80b9719268529c500e28193fd97b224e8e
springcloud <layoomielie@gmail.com>
2019-06-28 10:48:15 +0800

    assembly打包

---
+++ src/main/java/com/example/SayHello.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author：张鸿建
+ * @time：2019/6/28 10:32
+ * @desc：
+ **/
+public class SayHello {
+    public static void main(String[] args) {
+        System.out.println("SayHello   .................");
+    }
+}
---
+++ src/main/java/com/example/SayHi.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @author：张鸿建
+ * @time：2019/6/28 10:43
+ * @desc：
+ **/
+public class SayHi {
+
+    public static void main(String[] args) {
+        System.out.println("SayHi   .................");
+    }
+}
